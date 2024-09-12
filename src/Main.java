4bcc6d6869d1c449d5a82fe7b5a8835c08b27223
Novocaine <93640765+MrNovocain@users.noreply.github.com>
2024-09-12 17:04:03 -0400

    try pull2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.doFizzBuzz(10);
+        FizzBuzz.doFizzBuzz(15);
         FizzBuzz.main(new String[0]);
     }
 }
