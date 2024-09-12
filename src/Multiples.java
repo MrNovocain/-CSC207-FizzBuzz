2d8b299ca2bc70ba88e13994e8d8dfd1792220c3
Novocaine <shane.dong@mail.utoronto.ca>
2024-09-12 17:24:11 -0400

    fin Multiples 1.0

---
+++ src/Multiples.java
@@ -0,0 +1,23 @@
+
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+        int count = 0;
+        while (i <= 999) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 && divisibleBy5) {
+                count++;
+            }
+            else {
+            if (divisibleBy3)
+                count++;
+            else if (divisibleBy5) {
+                count++;
+            }}
+            i++;
+        }
+        System.out.println(count);
+    }
+}
